06a242ccb417d9a11f7477245f2abf525df9042a
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-01-03 14:44:59 +0200

    * limit support for Visual Studio from 2010 to 2015
    at least libsndfile needs <stdint.h> header but older versions of Visual Studio don't have it
    workaround may be introduced later

--- scripts/vsvars.cmd
+++ scripts/vsvars.cmd
@@ -1,6 +1,9 @@
 @echo off
 
-for %%v in ("%VS140COMNTOOLS%" "%VS120COMNTOOLS%" "%VS110COMNTOOLS%" "%VS100COMNTOOLS%" "%VS90COMNTOOLS%" "%VS80COMNTOOLS%") do (
+::  The same idea as in CMake:
+::  Check for the newest Visual Studio installed and setup its build environment
+
+for %%v in ("%VS140COMNTOOLS%" "%VS120COMNTOOLS%" "%VS110COMNTOOLS%" "%VS100COMNTOOLS%") do (
     call "%%~vvsvars32.bat" 1>nul 2>nul
 
     if %ERRORLEVEL% equ 0 (
