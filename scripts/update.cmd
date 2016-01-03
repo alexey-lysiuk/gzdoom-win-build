b9c92df16d4e2d54e82856952584e7eb4a860f18
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-01-03 10:57:13 +0200

    + update script: create local branches after cloning
    ZDoom's master, GZDoom's master (version 2.x.x) and GZDoom's old renderer (version 1.8.x)

--- scripts/update.cmd
+++ scripts/update.cmd
@@ -7,13 +7,20 @@ set PATH=%ROOT_DIR%thirdparty\git\bin;%PATH%
 
 if not exist gzdoom (
     git clone https://github.com/coelckers/gzdoom.git
-    cd gzdoom
-    git remote add rheit https://github.com/rheit/zdoom.git
-    cd ..
-)
 
-cd gzdoom
-git fetch --all
-git pull
+    cd gzdoom
+    git remote rename origin gzdoom
+    git remote add zdoom https://github.com/rheit/zdoom.git
+    git fetch --all
+    git branch --track gz_master gzdoom/master
+    git branch --track gz_18x gzdoom/g1.8
+    git branch --track z_master zdoom/master
+    git checkout gz_master
+    git branch --delete master
+) else (
+    cd gzdoom
+    git fetch --all
+    git pull
+)
 
 popd
