71077a88bf6019b79bdd0e5e7c452973b677ef76
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-01-03 14:38:43 +0200

    * proper check for Visual Studio path

--- scripts/vsvars.cmd
+++ scripts/vsvars.cmd
@@ -3,7 +3,7 @@
 for %%v in ("%VS140COMNTOOLS%" "%VS120COMNTOOLS%" "%VS110COMNTOOLS%" "%VS100COMNTOOLS%" "%VS90COMNTOOLS%" "%VS80COMNTOOLS%") do (
     call "%%~vvsvars32.bat" 1>nul 2>nul
 
-    if errorlevel 0 (
+    if %ERRORLEVEL% equ 0 (
         exit /b
     )
 )
