b6280d11b1013b83c5c078296009c21e9131871b
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-01-04 16:55:08 +0200

    * configure remotes/branches only when needed
    switch version script adds remotes and checkouts branches requested by user
    update script is no longer fetches all remotes and doesn't create local branches

--- scripts/update.cmd
+++ scripts/update.cmd
@@ -1,26 +1,11 @@
 @echo off
 
-set ROOT_DIR=%~dp0\..\
-pushd "%ROOT_DIR%"
+pushd "%~dp0"
 
-set PATH=%ROOT_DIR%thirdparty\git\bin;%PATH%
+call git_path.cmd
+call setup.cmd
 
-if not exist gzdoom (
-    git clone https://github.com/coelckers/gzdoom.git
-
-    cd gzdoom
-    git remote rename origin gzdoom
-    git remote add zdoom https://github.com/rheit/zdoom.git
-    git fetch --all
-    git branch --track gz_master gzdoom/master
-    git branch --track gz_18x gzdoom/g1.8
-    git branch --track z_master zdoom/master
-    git checkout gz_master
-    git branch --delete master
-) else (
-    cd gzdoom
-    git fetch --all
-    git pull
-)
+cd ..\gzdoom
+git pull
 
 popd
--- switch_version.cmd
+++ switch_version.cmd
@@ -1,5 +1,8 @@
 @echo off
 
+set REMOTE_NAME=
+set REMOTE_URL=
+
 echo. 
 echo ^+----------------------------------------------------------^+
 echo ^|  Press the corresponding button to switch to a version:  ^|
@@ -15,11 +18,16 @@ echo.
 choice /c 1230
 
 if %ERRORLEVEL% equ 1 (
-    set BRANCH=gz_master
+    set BRANCH_LOCAL=gz_master
+    set BRANCH_REMOTE=gzdoom/master
 ) else if %ERRORLEVEL% equ 2 (
-    set BRANCH=gz_18x
+    set BRANCH_LOCAL=gz_18x
+    set BRANCH_REMOTE=gzdoom/g1.8
 ) else if %ERRORLEVEL% equ 3 (
-    set BRANCH=z_master
+    set REMOTE_NAME=zdoom
+    set REMOTE_URL=https://github.com/rheit/zdoom.git
+    set BRANCH_LOCAL=z_master
+    set BRANCH_REMOTE=zdoom/master
 ) else (
     exit /b
 )
@@ -27,12 +35,48 @@ if %ERRORLEVEL% equ 1 (
 pushd "%~dp0"
 
 echo.
-call scripts\update.cmd
+call scripts\git_path.cmd
+call scripts\setup.cmd
 
 cd gzdoom
-git checkout %BRANCH%
+git fetch --all
+
+if defined REMOTE_NAME (
+    call :AddRemote %REMOTE_NAME% %REMOTE_URL%
+)
+
+call :CheckoutBranch %BRANCH_LOCAL% %BRANCH_REMOTE%
 
 popd
 
 echo.
 pause
+
+goto :eof
+
+:AddRemote
+    :: %1 - remote name
+    :: %2 - remote url
+    :: 'more' converts Unix to DOS line endings, i.e. <LF> to <CR><LF>
+    git remote -v | more | findstr \^<%1\^> >nul
+
+    if %ERRORLEVEL% neq 0 (
+        git remote add -f %1 %2
+    )
+
+    goto :eof
+
+:CheckoutBranch
+    :: %1 - local branch name
+    :: %2 - remote branch name
+    :: 'more' converts Unix to DOS line endings, i.e. <LF> to <CR><LF>
+    git branch -v | more | findstr \^<%1\^> >nul
+
+    if %ERRORLEVEL% neq 0 (
+        git branch --track %1 %2
+    )
+
+    echo git checkout %1
+    git checkout %1
+
+    goto :eof
