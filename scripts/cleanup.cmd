c1a184da0aef404d7cbdbaa39bf716862c2a875a
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-01-07 11:23:23 +0200

    + script for cleanup after branch switching
    deletes CMake cached values and files generated by build process

---
+++ scripts/cleanup.cmd
@@ -0,0 +1,29 @@
+@echo off
+
+if not "%1" == "86" if not "%1" == "64" (
+    echo Specify 86 or 64 as the first argument.
+    exit /b
+)
+
+pushd "%~dp0"
+
+cd ..\build_x%1
+del CMakeCache.txt 1>nul 2>nul
+
+for %%d in (Debug Release MinSizeRel RelWithDebInfo) do (
+    call :DeleteGeneratedFiles %%d
+)
+
+cd ..\bin_x%1
+call :DeleteGeneratedFiles .
+
+popd
+
+goto :eof
+
+:DeleteGeneratedFiles
+    del %1\*.exe 1>nul 2>nul
+    del %1\*.pdb 1>nul 2>nul
+    del %1\*.pk3 1>nul 2>nul
+
+    goto :eof
