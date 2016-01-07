10cd66607120d78d8971c6d96c2c03bb088a233d
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-01-07 11:59:02 +0200

    + deploy script
    makes x86/x64 builds and creates .zip archives ready to deploy

---
+++ deploy.cmd
@@ -0,0 +1,32 @@
+@echo off
+
+pushd "%~dp0scripts"
+
+call git_path.cmd
+call build.cmd 86
+call build.cmd 64
+
+cd ../gzdoom
+set BUILD_BRANCH=
+set BUILD_VERSION=
+for /f "delims=" %%r in ('git rev-parse --abbrev-ref HEAD') do set BUILD_BRANCH=%%r
+for /f "delims=" %%r in ('git describe --tags') do set BUILD_VERSION=%%r
+cd ..
+
+if not exist deploy (
+    md deploy
+)
+
+call :MakeDeployZip 86
+call :MakeDeployZip 64
+
+popd
+
+echo.
+pause
+
+goto :eof
+
+:MakeDeployZip
+    thirdparty\zip\zip.exe -9j deploy\%BUILD_BRANCH%-x%1-%BUILD_VERSION%.zip bin_x%1\*.exe bin_x%1\*.dll bin_x%1\*.pk3
+    goto :eof
