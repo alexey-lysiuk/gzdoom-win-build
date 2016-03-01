79a4e87415fb5cf78e3922448bf7621ef6639fc7
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-03-01 16:36:23 +0200

    - detection for VS2010 and 2012
    these versions of Visual Studio cannot be used as their support for C++11 is insufficient

--- scripts/vsvars.cmd
+++ scripts/vsvars.cmd
@@ -4,20 +4,16 @@ if defined CMAKE_TOOLSET (
     goto :eof
 )
 
-set VS_COMNTOOLS_2010=%VS100COMNTOOLS%
-set VS_COMNTOOLS_2012=%VS110COMNTOOLS%
 set VS_COMNTOOLS_2013=%VS120COMNTOOLS%
 set VS_COMNTOOLS_2015=%VS140COMNTOOLS%
 
-set VS_TOOLSET_2010=v100
-set VS_TOOLSET_2012=v110_xp
 set VS_TOOLSET_2013=v120_xp
 set VS_TOOLSET_2015=v140_xp
 
 ::  The same idea as in CMake:
 ::  Check for the newest Visual Studio installed and setup its build environment
 
-for %%v in (2015 2013 2012 2010) do (
+for %%v in (2015 2013) do (
     call :SetupEnvironment %%v
 
     if defined CMAKE_TOOLSET (
