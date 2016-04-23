efd6015f4414067be318d7a54eea04163eee6a54
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-04-23 10:04:28 +0300

    + cleanup script which removes build directories

---
+++ cleanup.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+choice /M "Are you sure to delete intermediate files"
+
+if %ERRORLEVEL% equ 1 (
+    cd "%~dp0"
+    rd /S /Q build_x86 2>nul
+    rd /S /Q build_x64 2>nul
+    echo Done.
+)
+
+pause
