9ab3776e5a6dcdfc9b708bda46d0e69e73db4d41
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-04-28 21:55:18 +0300

    * delete clone directory in cleanup script

--- cleanup.cmd
+++ cleanup.cmd
@@ -4,6 +4,7 @@ choice /M "Are you sure to delete intermediate files"
 
 if %ERRORLEVEL% equ 1 (
     cd "%~dp0"
+    rd /S /Q gzdoom 2>nul
     rd /S /Q build_x86 2>nul
     rd /S /Q build_x64 2>nul
     echo Done.
